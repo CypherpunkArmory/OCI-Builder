a997ee75844effa7bdaf2c4d78ffae914ff910f8
Corbin Champion <corbinlc@gmail.com>
2026-07-10 22:32:22 -0700

    Add XFCE and LXDE desktop variants for Debian, Kali, and Ubuntu
    Mirrors UserLAnd-Assets-{Debian,Kali,Ubuntu}_{XFCE,LXDE}: installs the
    desktop metapackage instead of twm, and writes a ~/.vnc/xstartup that
    launches startxfce4/startlxde. Image names use an underscore
    (userland-debian_xfce) to match how OciImageFetcher/ServerService.kt in
    the UserLAnd app construct the OCI image name from distributionType and
    flavor.

---
+++ assets/xfce/startVNCServerStep2.sh
@@ -0,0 +1,55 @@
+#! /bin/bash
+
+if [[ -z "${INITIAL_USERNAME}" ]]; then
+  INITIAL_USERNAME="user"
+fi
+
+if [[ -z "${INITIAL_VNC_PASSWORD}" ]]; then
+  INITIAL_VNC_PASSWORD="userland"
+fi
+
+if [ ! -f /home/$INITIAL_USERNAME/.vnc/passwd ]; then
+
+prog=/usr/bin/vncpasswd
+
+/usr/bin/expect <<EOF
+spawn "$prog"
+expect "Password:"
+send "$INITIAL_VNC_PASSWORD\r"
+expect "Verify:"
+send "$INITIAL_VNC_PASSWORD\r"
+expect "(y/n)?"
+send "n\r"
+expect eof
+exit
+EOF
+
+fi
+
+if [[ -z "${DIMENSIONS}" ]]; then
+	DIMENSIONS="1024x768"
+fi
+
+vncrc_line="\$geometry = \"${DIMENSIONS}\";"
+echo $vncrc_line > /home/$INITIAL_USERNAME/.vncrc
+
+mkdir -p /home/$INITIAL_USERNAME/.vnc
+echo '#!/bin/sh' > /home/$INITIAL_USERNAME/.vnc/xstartup
+echo '/usr/bin/startxfce4' >> /home/$INITIAL_USERNAME/.vnc/xstartup
+chmod 777 /home/$INITIAL_USERNAME/.vnc/xstartup
+
+if [[ -z "${VNC_DISPLAY}" ]]; then
+  VNC_DISPLAY="51"
+fi
+
+rm /tmp/.X${VNC_DISPLAY}-lock
+rm /tmp/.X11-unix/X${VNC_DISPLAY}
+tightvncserver -kill :${VNC_DISPLAY}
+tightvncserver :${VNC_DISPLAY}
+
+while [ ! -f /home/$INITIAL_USERNAME/.vnc/localhost:${VNC_DISPLAY}.pid ]
+do
+  sleep 1
+done
+cd ~
+DISPLAY=:${VNC_DISPLAY} xterm -geometry 80x24+0+0 -e /bin/bash --login &
